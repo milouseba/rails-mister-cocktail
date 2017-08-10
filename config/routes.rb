f0ac3fbcd30ae07c0ad81e77ae216a7687268a97
sebastien milou <milouseba@gmail.com>
2017-08-10 16:38:21 +0200

    structure without style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+
+  root to: 'cocktails#index'
+
+  resources :cocktails do
+    resources :doses, only: [ :index, :show, :new, :create ]
+  end
+
+  resources :doses, only: [ :new, :create, :destroy ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
