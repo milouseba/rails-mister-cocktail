d3480a1b05f9b6391ad570088e51a708cc268725
sebastien milou <milouseba@gmail.com>
2017-08-10 12:48:50 +0200

    validators

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, :cocktail_id, :ingredient_id, presence: true
+  validates_uniqueness_of :cocktail_id, :scope => :ingredient_id
+  # validates [:cocktail_id, :ingredient_id], uniqueness: true
 end
